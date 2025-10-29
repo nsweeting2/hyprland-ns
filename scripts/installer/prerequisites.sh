72c0d996add3f25a583550ab1003a84709b32a7f
Noah Sweeting <nsweeting2@gmail.com>
2025-10-28 21:33:06 -0400

    pass 2

--- scripts/installer/prerequisites.sh
+++ scripts/installer/prerequisites.sh
@@ -10,7 +10,7 @@ log_message "Installation started for prerequisites section"
 print_info "\nStarting prerequisites setup..."
 echo "------------------------------------------------------------------------"
 
-run_command "pacman -Syyu --noconfirm" "Update package database and upgrade packages (Recommended)" "yes" # no
+run_command "pacman -Syyu --noconfirm" "Update package database and upgrade packages" "yes" # no
 
 if command -v yay > /dev/null; then
     print_info "Skipping yay installation (already installed)."
@@ -19,24 +19,27 @@ elif run_command "pacman -S --noconfirm --needed git base-devel" "Install YAY (M
     run_command "makepkg --noconfirm -si && cd .. # builds with makepkg" "Build YAY (Must)/Breaks the script" "no" "no" 
 fi
 
-run_command "pacman -S --noconfirm iwd" "Install IWD for Wi-Fi management (Recommended)" "yes"
+run_command "pacman -S --noconfirm iwd" "Install and IWD with Impala for Wi-Fi networking" "yes"
 
-run_command "pacman -S --noconfirm bluez bluez-utils" "Install BlueZ for Bluetooth support (Recommended)" "yes"
+run_command "pacman -S --noconfirm bluez bluez-utils" "Install BlueZ with BlueTUI for Bluetooth support" "yes"
 
-run_command "pacman -S --noconfirm pipewire wireplumber" "Install Pipewire and Wireplumber for audio control (Recommended)" "yes" 
+run_command "pacman -S --noconfirm pipewire wireplumber" "Install Pipewire, Wireplumber, and PulseAudio for audio functionality" "yes"
 
-run_command "pacman -S --noconfirm brightnessctl" "Install brightnessctl for backlight control (Recommended)" "yes"
+run_command "yay -S --sudoloop --noconfirm jvol-git" "Install Jvol for audio controls" "yes" "no"
+
+#I want a TUI or sorts for brightness control....
+run_command "pacman -S --noconfirm brightnessctl" "Install brightnessctl for backlight control" "yes"
 
 run_command "pacman -S --noconfirm ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono" "Installing Nerd Fonts and Symbols (Recommended)" "yes" 
 
-run_command "pacman -S --noconfirm sddm && systemctl enable sddm.service" "Install and enable SDDM (Recommended)" "yes"
+#run_command "pacman -S --noconfirm sddm && systemctl enable sddm.service" "Install and enable SDDM" "yes"
 
 run_command "yay -S --sudoloop --noconfirm brave-bin" "Install Brave Browser" "yes" "no"
 
-run_command "pacman -S --noconfirm kitty ghostty" "Install Kitty & Ghostty - Terminal Emulators (Recommended)" "yes"
+run_command "pacman -S --noconfirm kitty ghostty" "Install Kitty & Ghostty - Terminal Emulators" "yes"
 
 run_command "pacman -S --noconfirm nano" "Install Nano and Neovim - Text Editors" "yes"
 
-run_command "pacman -S --noconfirm tar 7zip" "Install tar and 7zip for extracting files (Must)/needed for copying themes" "yes"
+run_command "pacman -S --noconfirm tar 7zip" "Install tar and 7zip for extracting files" "yes"
 
 echo "------------------------------------------------------------------------"
